60bd729b4c036fcdb4d23efc4a9b0a9494b4f9ce
LIU Xiao <david.liu.chn@gmail.com>
2024-03-06 22:36:54 +0100

    add langsmith

--- rag_engine.py
+++ rag_engine.py
@@ -6,7 +6,6 @@ import PyPDF2
 
 from langchain.chains import RetrievalQA, ConversationalRetrievalChain
 from langchain.embeddings import OpenAIEmbeddings
-# from langchain.vectorstores import Chroma
 import chromadb
 from langchain import OpenAI
 from langchain.llms.openai import OpenAIChat
@@ -18,36 +17,69 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.memory import ConversationBufferMemory
 from langchain.memory.chat_message_histories import StreamlitChatMessageHistory
 
+from langchain_core.prompts.prompt import PromptTemplate
+
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+# from langchain_core.output_parsers import StrOutputParser
+
 import streamlit as st
+from streamlit_js_eval import streamlit_js_eval
 # import shutil
 
-import os
+#SeleniumURLLoader
+from langchain.document_loaders import SeleniumURLLoader
 
+#validate results
+from sentence_transformers import SentenceTransformer, util
+
+
+import os
+os.environ["OPENAI_API_KEY"] = 'sk-'
+os.environ["LANGCHAIN_TRACING_V2"] = 'true'
+os.environ["LANGCHAIN_API_KEY"] = ''
 
 TMP_DIR = Path(__file__).resolve().parent.joinpath('data', 'tmp')
 LOCAL_VECTOR_STORE_DIR = Path(__file__).resolve().parent.joinpath('data', 'vector_store')
 
-st.set_page_config(page_title="RAG")
-st.title("AI in BIS test")
+st.set_page_config(page_title="RAG Demo")
+st.title("RAG demo")
 
+#TODO clean code
+# output_parser = StrOutputParser()
 
 def load_documents():
     loader = DirectoryLoader(TMP_DIR.as_posix(), glob='**/*.pdf')
     documents = loader.load()
     return documents
+# def load_documents():
+#     loader = DirectoryLoader(TMP_DIR.as_posix(), glob='**/*.pdf')
+#     documents = loader.load()
+#     return documents
 
 def clean_documents():
     # shutil.rmtree(LOCAL_VECTOR_STORE_DIR.as_posix())
     # vectordb = chromadb.PersistentClient(path=LOCAL_VECTOR_STORE_DIR.as_posix(), settings=chromadb.config.Settings(anonymized_telemetry=False, allow_reset=True))
     # vectordb.reset()
+    st.session_state.client.reset()
     st.session_state.retriever = None
+    st.session_state.source_docs = None
+    streamlit_js_eval(js_expressions="parent.window.location.reload()")
     return
 
 def split_documents(documents):
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    text_splitter = CharacterTextSplitter(chunk_size=2000, chunk_overlap=50)
     texts = text_splitter.split_documents(documents)
     return texts
 
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+def split_links(url, title):
+    cleaned_url = url.strip("b'").strip('"')
+    loader = SeleniumURLLoader(urls=[cleaned_url])
+    documents = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=50)
+    all_splits = text_splitter.split_documents(documents)
+    return all_splits
 def embeddings_on_local_vectordb(texts):
     print("local")
     vectordb = Chroma.from_documents(texts, embedding=OpenAIEmbeddings(), client=st.session_state.client,
@@ -65,10 +97,13 @@ def embeddings_on_pinecone(texts):
     return retriever
 
 def query_llm(retriever, query):
+    # PROMPT = PromptTemplate.from_template(_template)
+
     qa_chain = ConversationalRetrievalChain.from_llm(
         # llm=OpenAIChat(openai_api_key=st.session_state.openai_api_key),
         llm=ChatOpenAI(model_name="gpt-3.5-turbo-16k", openai_api_key=st.session_state.openai_api_key),
         # llm=OpenAI(model_name="gpt-3.5-turbo-16k"),
+        # condense_question_prompt = PROMPT,
         retriever=retriever,
         return_source_documents=True,
         return_generated_question=True,
@@ -105,6 +140,7 @@ def input_fields():
     #
     # print("finish init")
     st.session_state.pinecone_db = st.toggle('Use Pinecone Vector DB')
+    st.button("Reset knowledge", on_click=clean_documents)
     # print("use pinecone: " +str(st.session_state.pinecone_db))
     #
     col1, col2 = st.columns(2)
@@ -117,15 +153,47 @@ def input_fields():
     with col2:
         st.session_state.links = st.file_uploader(label="Load webpages", type="properties", accept_multiple_files=False)
         st.button("Submit links", on_click=process_links)
+
+
+    # init Chroma DB TODO do we need pinecone ?
+    st.session_state.client = chromadb.PersistentClient(path=LOCAL_VECTOR_STORE_DIR.as_posix(),
+                                                        settings=chromadb.config.Settings(
+                                                            anonymized_telemetry=False,
+                                                            allow_reset=True))
     # if hasattr(st.session_state, 'source_docs'):
     #     print("docs are1: " + str(st.session_state.source_docs))
     #
+
+
+def process_links():
+    if not st.session_state.openai_api_key or not st.session_state.pinecone_api_key or not st.session_state.source_docs:
+        st.warning(f"Please upload the documents and provide the missing fields.")
+    try:
+        properties = {}
+        for line in st.session_state.links:
+            #
+            print("start load links: ")
+            url, title = str(line).strip().split('|')
+            properties[url.strip()] = title.strip()
+            texts = split_links(url, title)
+            print("split_links" + str(texts))
+            #
+            if not st.session_state.pinecone_db:
+                st.toast(f"Learning: "+ title.strip().lstrip("b'").rstrip("'"))
+                st.session_state.retriever = embeddings_on_local_vectordb(texts)
+            else:
+                st.info(f"storing in pinecone")
+                st.session_state.retriever = embeddings_on_pinecone(texts)
+    except Exception as e:
+        print("error: " + str(e))
+        st.error(f"An error occurred: {e}")
+    return
 
 def prepare_doc(pdf_docs):
     docs = []
     metadata = []
     content = []
-
+    # content_metadata_list = []
     for pdf in pdf_docs:
         print(pdf.name)
         pdf_reader = PyPDF2.PdfReader(pdf)
@@ -134,14 +202,21 @@ def prepare_doc(pdf_docs):
                         'content': pdf_reader.pages[index].extract_text()}
             docs.append(doc_page)
     for doc in docs:
+        st.toast(f"Learning: " + doc["title"])
+        # content_metadata_list.append((doc["content"], {"title": doc["title"]}))
         content.append(doc["content"])
         metadata.append({
             "title": doc["title"]
         })
     return content, metadata
-
-def process_links():
-    return
+def get_text_chunks(content, metadata):
+    text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+        chunk_size=2000,
+        chunk_overlap=50,
+    )
+    split_docs = text_splitter.create_documents(content, metadatas=metadata)
+    print(f"Documents are split into {len(split_docs)} passages")
+    return split_docs
 
 def process_documents():
     # if not st.session_state.openai_api_key or not st.session_state.pinecone_api_key or not st.session_state.pinecone_env or not st.session_state.pinecone_index or not st.session_state.source_docs:
@@ -149,34 +224,49 @@ def process_documents():
         st.warning(f"Please upload the documents and provide the missing fields.")
     else:
         try:
-            clean_documents()
-            st.session_state.client = chromadb.PersistentClient(path=LOCAL_VECTOR_STORE_DIR.as_posix(), settings=chromadb.config.Settings(anonymized_telemetry=False, allow_reset=True))
-            st.session_state.client.reset()
-            for source_doc in st.session_state.source_docs:
-                #
-                print("start load docs: ")
-                with tempfile.NamedTemporaryFile(prefix=source_doc.name, delete=False, dir=TMP_DIR.as_posix(), suffix='.pdf') as tmp_file:
-                    tmp_file.write(source_doc.read())
-                #
-                documents = load_documents()
-                print("load docs: " + str(documents))
-                #
-                for _file in TMP_DIR.iterdir():
-                    temp_file = TMP_DIR.joinpath(_file)
-                    temp_file.unlink()
-                #
-                texts = split_documents(documents)
-                print("split_documents" + str(texts))
-                #
-                if not st.session_state.pinecone_db:
-                    st.info(f"storing in local")
-                    st.session_state.retriever = embeddings_on_local_vectordb(texts)
-                else:
-                    st.info(f"storing in pinecone")
-                    st.session_state.retriever = embeddings_on_pinecone(texts)
+            print("start load docs: ")
+            content, metadata = prepare_doc(st.session_state.source_docs)
+            split_docs = get_text_chunks(content, metadata)
+            if not st.session_state.pinecone_db:
+                st.session_state.retriever = embeddings_on_local_vectordb(split_docs)
+            else:
+                st.info(f"storing in pinecone")
+                st.session_state.retriever = embeddings_on_pinecone(texts)
+
+
+            # for content, metadata in content_metadata_list:
+            #     #
+            #     title = metadata.get("title")
+            #     #
+            #     print("load docs: " + title)
+            #     #
+            #     texts = get_text_chunks(content, metadata)
+            #     print("split_documents" + str(texts))
+            #     #
+            #     if not st.session_state.pinecone_db:
+            #         st.info(f"Learning: "+ title)
+            #         st.session_state.retriever = embeddings_on_local_vectordb(texts)
+            #     else:
+            #         st.info(f"storing in pinecone")
+            #         st.session_state.retriever = embeddings_on_pinecone(texts)
         except Exception as e:
             print("error: " + str(e))
             st.error(f"An error occurred: {e}")
+
+def validate_answer_against_sources(response_answer, source_documents):
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+    similarity_threshold = 0.5
+    source_texts = [doc.page_content for doc in source_documents]
+
+    answer_embedding = model.encode(response_answer, convert_to_tensor=True)
+    source_embeddings = model.encode(source_texts, convert_to_tensor=True)
+
+    cosine_scores = util.pytorch_cos_sim(answer_embedding, source_embeddings)
+
+    if any(score.item() > similarity_threshold for score in cosine_scores[0]):
+        return True
+
+    return False
 
 def boot():
     #
@@ -193,12 +283,22 @@ def boot():
     if query := st.chat_input():
         st.chat_message("human").write(query)
         response = query_llm(st.session_state.retriever, query)
-        col1, col2 = st.columns(2)
+
+        # Post-processing step to validate the answer against the source documents
+        if response['source_documents']:
+            response_answer = response['answer']
+            source_docs = response['source_documents']
+        is_valid_answer = validate_answer_against_sources(response_answer, source_docs)
+
+        col1, col2 = st.columns([3, 1])
         with col1:
             st.chat_message("ai").write(response['answer'])
-        with col2:
-            st.chat_message("code").write(response['source_documents'])
+        if is_valid_answer:
+            with col2:
+                st.chat_message("code").write(response['source_documents'])
 
+
+#TODO add validation of source ref
 if __name__ == '__main__':
     #
     boot()
