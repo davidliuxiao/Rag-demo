0ffe26dff770e30ccb6d6dfa1919e6394c324d70
LIU Xiao <david.liu.chn@gmail.com>
2024-03-07 02:02:18 +0100

    apply style

--- rag_engine.py
+++ rag_engine.py
@@ -28,16 +28,19 @@ from langchain.document_loaders import SeleniumURLLoader
 from sentence_transformers import SentenceTransformer, util
 
 
-import os
-os.environ["OPENAI_API_KEY"] = 'sk-'
 os.environ["LANGCHAIN_TRACING_V2"] = 'true'
-os.environ["LANGCHAIN_API_KEY"] = ''
+os.environ["LANGCHAIN_API_KEY"] = 'ls__3c556b0468344b198cc40b30da61f447'
 
 TMP_DIR = Path(__file__).resolve().parent.joinpath('data', 'tmp')
 LOCAL_VECTOR_STORE_DIR = Path(__file__).resolve().parent.joinpath('data', 'vector_store')
 
+
+
 st.set_page_config(page_title="RAG Demo")
-st.title("RAG demo")
+
+with open("./static/style.css") as f:
+    st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
+
 
 #TODO clean code
 # output_parser = StrOutputParser()
@@ -57,7 +60,7 @@ def clean_documents():
     # vectordb.reset()
     with st.spinner(text='In progress'):
         time.sleep(3)
-        st.success('Done')
+        st.success('Knowledge Base Initialized')
     st.session_state.client.reset()
     st.session_state.retriever = None
     st.session_state.source_docs = None
@@ -98,7 +101,7 @@ def query_llm(retriever, query):
 
     qa_chain = ConversationalRetrievalChain.from_llm(
         # llm=OpenAIChat(openai_api_key=st.session_state.openai_api_key),
-        llm=ChatOpenAI(model_name="gpt-3.5-turbo-16k", openai_api_key=st.session_state.openai_api_key),
+        llm=ChatOpenAI(model_name=st.session_state.llm_model, openai_api_key=st.session_state.openai_api_key),
         # llm=OpenAI(model_name="gpt-3.5-turbo-16k"),
         # condense_question_prompt = PROMPT,
         retriever=retriever,
@@ -112,8 +115,12 @@ def query_llm(retriever, query):
 
 def input_fields():
     print("start init")
+    st.session_state.openai_api_key = st.secrets.openai_api_key
     #
-    # with st.sidebar:
+    with st.sidebar:
+        st.session_state.llm_model = st.selectbox('Model', ['gpt-3.5-turbo-0125','gpt-4-0125-preview'])
+        st.container(height=100, border=False)
+        st.button("Reset knowledge", on_click=clean_documents)
     #     #
     #     if "openai_api_key" in st.secrets:
     #         st.session_state.openai_api_key = st.secrets.openai_api_key
@@ -137,7 +144,7 @@ def input_fields():
     #
     # print("finish init")
     # st.session_state.pinecone_db = st.toggle('Use Pinecone Vector DB')
-    st.button("Reset knowledge", on_click=clean_documents)
+
     # print("use pinecone: " +str(st.session_state.pinecone_db))
     #
     col1, col2 = st.columns(2)
@@ -163,7 +170,7 @@ def input_fields():
 
 
 def process_links():
-    if not st.session_state.openai_api_key or not st.session_state.pinecone_api_key or not st.session_state.source_docs:
+    if not st.session_state.openai_api_key or not st.session_state.source_docs:
         st.warning(f"Please upload the documents and provide the missing fields.")
     try:
         properties = {}
@@ -175,12 +182,15 @@ def process_links():
             texts = split_links(url, title)
             print("split_links" + str(texts))
             #
-            if not st.session_state.pinecone_db:
-                st.toast(f"Learning: "+ title.strip().lstrip("b'").rstrip("'"))
-                st.session_state.retriever = embeddings_on_local_vectordb(texts)
-            else:
-                st.info(f"storing in pinecone")
+            st.toast(f"Learning: " + title.strip().lstrip("b'").rstrip("'"))
+            st.session_state.retriever = embeddings_on_local_vectordb(texts)
+            # if not st.session_state.pinecone_db:
+            #     st.toast(f"Learning: "+ title.strip().lstrip("b'").rstrip("'"))
+            #     st.session_state.retriever = embeddings_on_local_vectordb(texts)
+            # else:
+            #     st.info(f"storing in pinecone")
                 # st.session_state.retriever = embeddings_on_pinecone(texts)
+        st.toast("Knowledge Base Updated")
     except Exception as e:
         print("error: " + str(e))
         st.error(f"An error occurred: {e}")
@@ -217,17 +227,22 @@ def get_text_chunks(content, metadata):
 
 def process_documents():
     # if not st.session_state.openai_api_key or not st.session_state.pinecone_api_key or not st.session_state.pinecone_env or not st.session_state.pinecone_index or not st.session_state.source_docs:
-    if not st.session_state.openai_api_key or not st.session_state.pinecone_api_key or not st.session_state.source_docs:
+    if not st.session_state.openai_api_key or not st.session_state.source_docs:
         st.warning(f"Please upload the documents and provide the missing fields.")
     else:
         try:
             print("start load docs: ")
             content, metadata = prepare_doc(st.session_state.source_docs)
             split_docs = get_text_chunks(content, metadata)
-            if not st.session_state.pinecone_db:
-                st.session_state.retriever = embeddings_on_local_vectordb(split_docs)
-            else:
-                st.info(f"storing in pinecone")
+            st.session_state.retriever = embeddings_on_local_vectordb(split_docs)
+            st.toast("Knowledge Base Updated")
+            # with st.spinner(text='In progress'):
+            #     time.sleep(1)
+            #     st.success('Knowledge Base Updated')
+            # if not st.session_state.pinecone_db:
+            #     st.session_state.retriever = embeddings_on_local_vectordb(split_docs)
+            # else:
+            #     st.info(f"storing in pinecone")
                 # st.session_state.retriever = embeddings_on_pinecone(texts)
 
 
@@ -246,29 +261,39 @@ def process_documents():
             #     else:
             #         st.info(f"storing in pinecone")
             #         st.session_state.retriever = embeddings_on_pinecone(texts)
+
         except Exception as e:
             print("error: " + str(e))
             st.error(f"An error occurred: {e}")
 
 def validate_answer_against_sources(response_answer, source_documents):
-    model = SentenceTransformer('all-MiniLM-L6-v2')
-    similarity_threshold = 0.5
-    source_texts = [doc.page_content for doc in source_documents]
-
-    answer_embedding = model.encode(response_answer, convert_to_tensor=True)
-    source_embeddings = model.encode(source_texts, convert_to_tensor=True)
-
-    cosine_scores = util.pytorch_cos_sim(answer_embedding, source_embeddings)
-
-    if any(score.item() > similarity_threshold for score in cosine_scores[0]):
-        return True
+    # model = SentenceTransformer('all-MiniLM-L6-v2')
+    # similarity_threshold = 0.5
+    # source_texts = [doc.page_content for doc in source_documents]
+    #
+    # answer_embedding = model.encode(response_answer, convert_to_tensor=True)
+    # source_embeddings = model.encode(source_texts, convert_to_tensor=True)
+    #
+    # cosine_scores = util.pytorch_cos_sim(answer_embedding, source_embeddings)
+    #
+    # if any(score.item() > similarity_threshold for score in cosine_scores[0]):
+    #     return True
 
     return False
+# Streamed response emulator
+def response_generator(response):
+    for word in response.split():
+        yield word + " "
+        time.sleep(0.05)
 
 def boot():
     #
     print("BOOT")
+    st.image('./static/bis.png', width=100)
+    st.title("BIS ChatBot")
     input_fields()
+    st.image('./static/genai.png', width=80)
+    st.subheader("How can I help you today?")
     #
     if "messages" not in st.session_state:
         st.session_state.messages = []
@@ -277,7 +302,7 @@ def boot():
         st.chat_message('human').write(message[0])
         st.chat_message('ai').write(message[1])    
     #
-    if query := st.chat_input():
+    if query := st.chat_input(placeholder='Message BISGPT...'):
         st.chat_message("human").write(query)
         response = query_llm(st.session_state.retriever, query)
 
@@ -285,14 +310,17 @@ def boot():
         if response['source_documents']:
             response_answer = response['answer']
             source_docs = response['source_documents']
-        is_valid_answer = validate_answer_against_sources(response_answer, source_docs)
+        # is_valid_answer = validate_answer_against_sources(response_answer, source_docs)
 
         col1, col2 = st.columns([3, 1])
         with col1:
-            st.chat_message("ai").write(response['answer'])
-        if is_valid_answer:
-            with col2:
-                st.chat_message("code").write(response['source_documents'])
+            # st.chat_message("ai").write(response['answer'])
+            st.chat_message("ai").write_stream(response_generator(response['answer']))
+        with col2:
+            st.chat_message("code").write(response['source_documents'])
+        # if is_valid_answer:
+        #     with col2:
+        #         st.chat_message("code").write(response['source_documents'])
 
 
 #TODO add validation of source ref
